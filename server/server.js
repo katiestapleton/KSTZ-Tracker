f5273081cad706f56491eebe133b6a9627731ce7
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-02-15 16:20:49 -0600

    added portfinder, removed hard-coded port
    added portfinder to server (finds the next available free port automatically).
    express testing removed. no longer needed.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
-//temporary: hard port will be changed to static
-const port=3005;
+const portfinder = require('portfinder');
 
-app.listen(port, function() {
-    console.log("Server is running on Port: " + port);
-  });
+// finds free port. renoves need for hard-coded port
+portfinder.getPort((err, port) => {
+    if (err) {
+     console.log(err)
+    } else {
+     app.listen(port, () => {
+      // console.log(`server started at http://${config.dev.host}:${port}`)
+      console.log(`Server is running on Port: http://localhost:${port}`)
+     })
+    }
+   })
+
 
 // connect to database via mongoose
-var uri = 'mongodb+srv://adm_user:twIfreTC6Arz4Ktt@trackerdb.dtouv.mongodb.net/default?retryWrites=true&w=majority'
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+var uriMdb = 'mongodb+srv://adm_user:twIfreTC6Arz4Ktt@trackerdb.dtouv.mongodb.net/default?retryWrites=true&w=majority'
+mongoose.connect(uriMdb, { useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once("open", function() {
